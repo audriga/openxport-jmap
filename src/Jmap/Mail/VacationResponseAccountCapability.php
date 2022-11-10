938208028a584ade7b32c5f534f2bf5d1d282fe0
Stanimir Bozhilov <stanimir@audriga.com>
2022-11-10 16:46:12 +0100

    Implement support for VacationResponse/get method

---
+++ src/Jmap/Mail/VacationResponseAccountCapability.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace OpenXPort\Jmap\Mail;
+
+use OpenXPort\Jmap\Core\AccountCapability;
+
+class VacationResponseAccountCapability extends AccountCapability
+{
+    public function __construct()
+    {
+        $this->capabilities = array();
+        $this->name = "urn:ietf:params:jmap:vacationresponse";
+    }
+}
