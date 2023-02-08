ce6dd273600e9a28dcc86a4ffe74061aa4b8e7b1
Joris Baum <joris@audriga.com>
2023-02-08 13:26:02 +0100

    Add AddressBook/set method for JSContacts
    Also bump lock file

---
+++ src/Jmap/JSContact/Methods/AddressBookSetMethod.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace OpenXPort\Jmap\JSContact\Methods;
+
+use OpenXPort\Jmap\Core\Methods\SetMethod;
+
+class AddressBookSetMethod extends SetMethod
+{
+    public function handle($methodCall, $dataAccessors, $dataAdapters, $dataMappers)
+    {
+        $arguments = $methodCall->getArguments();
+        $methodName = $methodCall->getName();
+        $adapter = $dataAdapters["AddressBooks"];
+        $mapper = $dataMappers["AddressBooks"];
+
+        if (isset($arguments["create"]) && !is_null($arguments["create"])) {
+            $bookMap = $mapper->mapFromJmap($arguments["create"], $adapter);
+            $created = $dataAccessors["AddressBooks"]->create($bookMap);
+        }
+        if (isset($arguments["destroy"]) && !is_null($arguments["destroy"])) {
+            $destroyed = $dataAccessors["AddressBooks"]->destroy($arguments["destroy"]);
+        }
+
+        return $this->buildMethodResponse($created, $destroyed, $methodCall);
+    }
+}
