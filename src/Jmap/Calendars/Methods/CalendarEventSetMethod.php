6c7ae5d2f444b9593b7c269a1e9d2c5a7dc5070a
Gerke Frölje <gerke@audriga.com>
2023-03-31 15:19:15 +0200

    Rework deserialization of Event in CalendarEventSetMethod

--- src/Jmap/Calendars/Methods/CalendarEventSetMethod.php
+++ src/Jmap/Calendars/Methods/CalendarEventSetMethod.php
@@ -17,8 +17,18 @@ class CalendarEventSetMethod extends SetMethod
         $destroyed = [];
 
         if (isset($arguments["create"]) && !is_null($arguments["create"])) {
-            $jsCalendarEvent = CalendarEvent::fromJson($arguments["create"]);
-            $calendarEventMap = $mapper->mapFromJmap($jsCalendarEvent, $adapter);
+            // Since we now support deserialization, we can use that here.
+            //
+            // This is a bit sketchy so I will rework this at some point.
+            //
+            // TODO: Since we now deserialize and serialize all over the place,
+            // it might be worth to consider doing this earlier
+            $calendarToCreate = get_object_vars($arguments["create"]);
+            $calendarId = array_keys($calendarToCreate)[0];
+            $jsCalendarEvent = CalendarEvent::fromJson($calendarToCreate[$calendarId]);
+            $jsCalendar = array($calendarId => $jsCalendarEvent);
+
+            $calendarEventMap = $mapper->mapFromJmap($jsCalendar, $adapter);
             $created = $dataAccessors["CalendarEvents"]->create($calendarEventMap);
         }
         if (isset($arguments["destroy"]) && !is_null($arguments["destroy"])) {
