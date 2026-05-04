eee5217dabd722e3aa0c9334fc576ff1a9044878
puja <pujakarmacharya17@gmail.com>
2026-05-04 16:39:09 +0200

    add update for calendar

--- src/Jmap/Calendars/Methods/CalendarSetMethod.php
+++ src/Jmap/Calendars/Methods/CalendarSetMethod.php
@@ -15,6 +15,7 @@ class CalendarSetMethod extends SetMethod
         $mapper = $dataMappers["Calendars"];
         $created = [];
         $destroyed = [];
+        $updated = [];
 
         if (isset($arguments["create"]) && !is_null($arguments["create"])) {
             $calendarsToCreate = $arguments["create"];
@@ -33,6 +34,44 @@ class CalendarSetMethod extends SetMethod
                 }
             }
         }
+
+        // Handle update operations
+        if (isset($arguments["update"]) && !is_null($arguments["update"])) {
+            $calendarsToUpdate = $arguments["update"];
+
+            foreach ($calendarsToUpdate as $id => $partialCalendarData) {
+                try {
+                    $existingCalendars = $dataAccessors["Calendars"]->get([$id]);
+
+                    if (empty($existingCalendars) || !isset($existingCalendars[$id])) {
+                        continue;
+                    }
+
+                    $existingCalendar = $existingCalendars[$id];
+                    $existingJsCalendar = $mapper->mapToJmap([$id => $existingCalendar], $adapter);
+
+                    if (empty($existingJsCalendar)) {
+                        continue;
+                    }
+
+                    $existingJsCalendar = reset($existingJsCalendar);
+                    $existingArray = json_decode(json_encode($existingJsCalendar), true);
+                    $updateArray = is_array($partialCalendarData) ? $partialCalendarData
+                        : json_decode(json_encode($partialCalendarData), true);
+
+                    $mergedArray = array_merge($existingArray, $updateArray);
+
+                    $updatedCalendars = $dataAccessors["Calendars"]->update([$id => $mergedArray]);
+
+                    if (isset($updatedCalendars[$id]) && $updatedCalendars[$id] === true) {
+                        $updated[$id] = (object)[];
+                    }
+                } catch (\Exception $e) {
+                    error_log("Failed to update calendar $id: " . $e->getMessage());
+                }
+            }
+        }
+
         // Handle destroy operations
         if (isset($arguments["destroy"]) && !is_null($arguments["destroy"])) {
             try {
@@ -41,6 +80,6 @@ class CalendarSetMethod extends SetMethod
                 error_log("Failed to destroy calendars: " . $e->getMessage());
             }
         }
-        return $this->buildMethodResponse($created, $destroyed, $methodCall);
+        return $this->buildMethodResponse($created, $destroyed, $methodCall, $updated);
     }
 }
