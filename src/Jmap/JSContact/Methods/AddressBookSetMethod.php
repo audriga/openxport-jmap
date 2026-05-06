04c97d3a579c9808045aee9d8d5ddd69c2edf355
puja <pujakarmacharya17@gmail.com>
2026-05-06 14:53:39 +0200

    add update in set for addressbook

--- src/Jmap/JSContact/Methods/AddressBookSetMethod.php
+++ src/Jmap/JSContact/Methods/AddressBookSetMethod.php
@@ -3,6 +3,7 @@
 namespace OpenXPort\Jmap\JSContact\Methods;
 
 use OpenXPort\Jmap\Core\Methods\SetMethod;
+use OpenXPort\Jmap\JSContact\AddressBook;
 
 class AddressBookSetMethod extends SetMethod
 {
@@ -13,16 +14,55 @@ class AddressBookSetMethod extends SetMethod
         $adapter = $dataAdapters["AddressBooks"];
         $mapper = $dataMappers["AddressBooks"];
         $created = [];
+        $updated = [];
         $destroyed = [];
 
         if (isset($arguments["create"]) && !is_null($arguments["create"])) {
             $bookMap = $mapper->mapFromJmap($arguments["create"], $adapter);
             $created = $dataAccessors["AddressBooks"]->create($bookMap);
         }
+
+        // Handle update operations
+        if (isset($arguments["update"]) && !is_null($arguments["update"])) {
+            $addressbooksToUpdate = $arguments["update"];
+
+            foreach ($addressbooksToUpdate as $id => $partialAddressbookData) {
+                try {
+                    $existingAddressbooks = $dataAccessors["AddressBooks"]->get([$id]);
+
+                    if (empty($existingAddressbooks) || !isset($existingAddressbooks[$id])) {
+                        continue;
+                    }
+
+                    $existingAddressbook = $existingAddressbooks[$id];
+                    $existingJsAddressbook = $mapper->mapToJmap([$id => $existingAddressbook], $adapter);
+
+                    if (empty($existingJsAddressbook)) {
+                        continue;
+                    }
+
+                    $existingJsAddressbook = reset($existingJsAddressbook);
+                    $existingArray = json_decode(json_encode($existingJsAddressbook), true);
+                    $updateArray = is_array($partialAddressbookData) ? $partialAddressbookData
+                        : json_decode(json_encode($partialAddressbookData), true);
+
+                    $mergedArray = array_merge($existingArray, $updateArray);
+
+                    $updatedAddressbooks = $dataAccessors["AddressBooks"]->update([$id => $mergedArray]);
+
+                    if (isset($updatedAddressbooks[$id]) && $updatedAddressbooks[$id] === true) {
+                        $updated[$id] = (object)[];
+                    }
+                } catch (\Exception $e) {
+                    error_log("Failed to update address book $id: " . $e->getMessage());
+                }
+            }
+        }
+
         if (isset($arguments["destroy"]) && !is_null($arguments["destroy"])) {
             $destroyed = $dataAccessors["AddressBooks"]->destroy($arguments["destroy"]);
         }
 
-        return $this->buildMethodResponse($created, $destroyed, $methodCall);
+        return $this->buildMethodResponse($created, $destroyed, $methodCall, $updated);
     }
 }
