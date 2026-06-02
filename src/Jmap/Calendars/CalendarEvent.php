3acfc09ea75143d974c116272c6edd4d06998d7a
puja <pujakarmacharya17@gmail.com>
2026-06-02 15:03:30 +0200

    Fix pref to string, addressBookIds jsonSerialize, calendarIds jsonSerialize, SetMethod response format

--- src/Jmap/Calendars/CalendarEvent.php
+++ src/Jmap/Calendars/CalendarEvent.php
@@ -633,7 +633,7 @@ class CalendarEvent extends JSCalendarDataType implements JsonSerializable
             "timeZone" => $this->getTimeZone(),
             "color" => $this->getColor(),
             "baseEventId" => $this->getBaseEventId(),
-            "calendarIds" => $this->getCalendarIds(),
+            "calendarIds" => $this->getCalendarIds() ? (object)$this->getCalendarIds() : null,
             "isDraft" => $this->getIsDraft(),
             "isOrigin" => $this->getIsOrigin(),
             "utcStart" => $this->getUtcStart(),
